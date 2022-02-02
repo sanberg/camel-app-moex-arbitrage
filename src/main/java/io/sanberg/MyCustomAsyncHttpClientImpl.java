780f52e749ceb1858b804d0de6bc26d74e60b50b
sanberg <eugenesanberg@gmail.com>
2022-02-03 02:26:56 +0300

    done: - code for alpaca websocket consuming and subscribing; - overriding prepareGet method in Async Http Client for auth; in progress: -processor and pojos for quotes and trades from alpaca US

--- src/main/java/io/sanberg/MyCustomAsyncHttpClientImpl.java
+++ src/main/java/io/sanberg/MyCustomAsyncHttpClientImpl.java
@@ -10,11 +10,9 @@ public class MyCustomAsyncHttpClientImpl extends DefaultAsyncHttpClient {
     public BoundRequestBuilder prepareGet(String url) {
         String id = "PKE5PQRGSKVQD6LQRJRK";
         String key = "yQ2lOJH8zFU1UVH4crFf0CMqe7Hmq7bTUTON4qX4";
-        // Headers for the handshake request
         HttpHeaders httpHeaders = new DefaultHttpHeaders();
         httpHeaders.set("Apca-Api-Key-Id", id);
         httpHeaders.set("Apca-Api-Secret-Key", key);
-        // Prepare the websocket upgrade handshake
         BoundRequestBuilder boundRequestBuilder = super.prepareGet(url);
         boundRequestBuilder.setHeaders(httpHeaders);
         return boundRequestBuilder;
